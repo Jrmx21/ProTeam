5a7397506e2c0f30762b53e4582eb8d2dc612334
David Ruiz Peña <119055850+Jrmx21@users.noreply.github.com>
2024-08-26 19:06:45 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ProTeam',
+  webDir: 'www'
+};
+
+export default config;
